32054b6424c2b722e901082caaeb408524789ac5
Manuel Illanes <rmib200@gmail.com>
2022-01-14 10:44:03 -0400

    added menu for training

--- Assets/_manuel/MyScripts/openbciConnection.cs
+++ Assets/_manuel/MyScripts/openbciConnection.cs
@@ -15,6 +15,8 @@ public class openbciConnection : MonoBehaviour
     
     public GameObject disconnect_btn;
 
+    //poner referencias al subject y session
+
     //public static double[,] data;
     // Start is called before the first frame update
     void startBoard()
